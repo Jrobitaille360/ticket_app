622e7713f1d08ad1b3f8d226389fb220557a80c4
J Robitaille <jrobitaille04@gmail.com>
2025-05-06 07:35:50 -0400

    0002 commit

--- lib/base/bottom_nav_bar.dart
+++ lib/base/bottom_nav_bar.dart
@@ -10,45 +10,45 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
+  final appScreens = [
+    const Text("Home"),
+    const Text("Search"),
+    const Text("Tickets"),
+    const Text("Profile"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(child: const Text("My Tickets",)),
-        ),
-        body: const Center(
-            child: Text(
-              ("Ticket Info"),
-            )
-        ),
-      bottomNavigationBar : BottomNavigationBar(
+      appBar: AppBar(title: Center(child: const Text("My Tickets"))),
+      body: Center(child: appScreens[0]),
+      bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.blueGrey,
         unselectedItemColor: const Color(0xFF526400),
         showSelectedLabels: false,
         items: [
           BottomNavigationBarItem(
-            label:"Home",
+            label: "Home",
             icon: Icon(FluentSystemIcons.ic_fluent_home_add_regular),
             activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
           ),
           BottomNavigationBarItem(
-            label:"Search",
+            label: "Search",
             icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
             activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
           ),
           BottomNavigationBarItem(
-            label:"Tickets",
+            label: "Tickets",
             icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
             activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
           ),
           BottomNavigationBarItem(
-            label:"Profile",
+            label: "Profile",
             icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
             activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
           ),
-
-        ]),
+        ],
+      ),
     );
-
   }
 }
